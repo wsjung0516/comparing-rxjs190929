528f4378a94432dc6975b95d4ea2eebf5f9ffbaf
wsjung-ikoob <wonsup.jung@ikoob.com>
2019-10-08 18:07:41 +0900

    refactoring

--- src/app/errors/errors-component/errors.component.spec.ts
+++ src/app/errors/errors-component/errors.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ErrorHandlerComponent } from './error-handler.component';
+import { ErrorsComponent } from './errors.component';
 
-describe('ErrorHandlerComponent', () => {
-  let component: ErrorHandlerComponent;
-  let fixture: ComponentFixture<ErrorHandlerComponent>;
+describe('ErrorsComponent', () => {
+  let component: ErrorsComponent;
+  let fixture: ComponentFixture<ErrorsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErrorHandlerComponent ]
+      declarations: [ ErrorsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ErrorHandlerComponent);
+    fixture = TestBed.createComponent(ErrorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/errors/errors-handler.spec.ts
@@ -0,0 +1,7 @@
+import { ErrorsHandler } from './errors-handler';
+
+describe('ErrorsHandler', () => {
+  it('should create an instance', () => {
+    expect(new ErrorsHandler()).toBeTruthy();
+  });
+});
