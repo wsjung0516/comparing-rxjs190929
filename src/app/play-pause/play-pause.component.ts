d75cd86e3f878a80acf6cd8d05c2df07b6ec6615
Wonsup Jung <wonsup.jung@gmail.com>
2019-12-09 11:23:29 +0900

    complete play pause function

--- src/app/play-pause/play-pause.component.ts
+++ src/app/play-pause/play-pause.component.ts
@@ -6,7 +6,7 @@ import {untilDestroyed} from 'ngx-take-until-destroy';
 @Component({
   selector: 'app-play-pause',
   template: `
-    <div style="width: 1000px" fxLayout="row" fxLayoutAlign="space-between center">
+    <div style="width: 1000px" fxLayout="row" fxLayoutAlign="left center" fxLayoutGap="5px">
       <button mat-raised-button color="primary" (click)="keyDown$.next(-1)">Key Down(Auto Up)</button>
       <button mat-raised-button color="primary" (click)="keyUp$.next(1)">Key Up(Auto Down)</button>
       <button mat-raised-button color="basic" (click)="keyLeft$.next(1)">Key Left(Speed Down)</button>
@@ -23,69 +23,45 @@ export class PlayPauseComponent implements OnInit, OnDestroy {
   keyLeft$ = new Subject();
   keyRight$ = new Subject();
   keySpace$ = new Subject<number>();
-  currentState;
+  interval$ = new Subject();
+  //
+  keyValue;
   oldState = 0;
   lastState = false;
-  currentSpeed: any = 1 ;
-  baseSpeed = 500;
-  curIdx = 5;
-  speed = [300,500,700,1000,1500,2000,2500,3000,4000,5000];
-  timer$: Observable<any>;
+  curIdx: any = 5;
+  interval;
+  speed = [300, 500, 700, 1000, 1500, 2000, 2500, 3000, 4000, 5000];
   constructor() { }
 
   ngOnInit() {
-         // this.baseSpeed = this.baseSpeed * this.currentSpeed;
-    this.curIdx = this.curIdx + this.currentSpeed;
-    if( this.curIdx > this.speed.length) this.curIdx = this.speed.length -1;
-    if( this.curIdx < 0 ) this.curIdx = 0;
-    //
     this.newSpeed();
-
-    // let timer1$ = timer(0, 200);
-    // let timer2$ = timer(0, 300);
-    // let timer3$ = timer(0, 500);
-    // let timer4$ = timer(0, 800);
-    // let timer5$ = timer(0, 1000);
-    // let timer6$ = timer(0, 2000);
-    // let timer7$ = timer(0, 3000);
-   // let timer$ = timer(0, 500 * this.currentSpeed);
-    //interval(500 * this.currentSpeed)
-    if( this.timer$ )this.timer$.pipe().subscribe();
-    this.newTimer().subscribe( val => console.log('timer', val));
-    //
     merge( this.keyUp$, this.keyDown$, this.keySpace$)
-      .pipe( untilDestroyed(this) ).subscribe( value => this.currentState = value);
+      .pipe( untilDestroyed(this) ).subscribe( value => this.keyValue = value);
     //
-    merge( this.keyLeft$, this.keyRight$)
-      .pipe( untilDestroyed(this),
+    merge( this.keyLeft$, this.keyRight$).pipe(
+        untilDestroyed(this),
         startWith(1)
-      )
-      .subscribe( value => {
-         this.currentSpeed = value;
-         console.log('currentSpeed', this.currentSpeed);
-    });
+      ).subscribe(  _=> this.newInterval());
     //
-/*
-    timer$.pipe(
-      map( _ => this.currentState ),
+    this.interval$.pipe(
+      map( _ => this.keyValue ),
       untilDestroyed(this),
     ).subscribe( value => console.log('result', value))
-*/
-
-
   }
-  newTimer() {
-    return this.timer$ = timer(0, this.speed[this.curIdx]).pipe();
+  //
+  newInterval() {
+    if ( this.interval ) clearInterval( this.interval);
+    return this.interval = setInterval(() => { this.interval$.next(); }, this.speed[this.curIdx]);
   }
   newSpeed() {
     return merge( this.keyLeft$, this.keyRight$)
       .pipe(
         untilDestroyed(this),
         map( value => {
-          this.curIdx = this.curIdx + (+value);
-          if( this.curIdx > this.speed.length) this.curIdx = this.speed.length -1;
+          this.curIdx = this.curIdx + value;
+          if( this.curIdx >= this.speed.length) this.curIdx = this.speed.length -1;
           if( this.curIdx < 0 ) this.curIdx = 0;
-          console.log('this.curIdx', this.curIdx);
+          console.log('interval', this.speed[this.curIdx]);
           return this.speed[this.curIdx];
 
         }),
@@ -96,17 +72,18 @@ export class PlayPauseComponent implements OnInit, OnDestroy {
     // console.log('this.currentState', this.currentState, this.lastState);
     this.lastState = !this.lastState;
     /** When called this function from KyeUp, keyDown, then need to change state*/
-    if( this.currentState !== 0) this.lastState = true;
+    if( this.keyValue !== 0) this.lastState = true;
     //
     if( this.lastState ) {
-      this.oldState = this.currentState;
-      this.currentState = 0;
+      this.oldState = this.keyValue;
+      this.keyValue = 0;
     } else {
-      this.currentState = this.oldState;
+      this.keyValue = this.oldState;
     }
     //
-    this.keySpace$.next(this.currentState);
+    this.keySpace$.next(this.keyValue);
   }
   ngOnDestroy(): void {
+    if ( this.interval ) clearInterval( this.interval);
   }
 }
